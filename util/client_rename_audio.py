6d21db8d024757ddb780f541fd7dba66a4b91a90
H1DDENADM1N <sudoomg@outlook.com>
2024-05-10 12:33:02 +0800

    语音控制回车换行

--- util/client_rename_audio.py
+++ util/client_rename_audio.py
@@ -14,19 +14,19 @@ def rename_audio(task_id, text, time_start) -> Union[Path, None]:
 
     # 确保旧文件存在
     if not file_path.exists():
-        console.print(f'    文件不存在：{file_path}')
+        console.print(f"    文件不存在：{file_path}")
         return
 
     # 构建新文件名
-    time_year = time.strftime('%Y', time.localtime(time_start))
-    time_month = time.strftime('%m', time.localtime(time_start))
+    time_year = time.strftime("%Y", time.localtime(time_start))
+    time_month = time.strftime("%m", time.localtime(time_start))
     time_ymdhms = time.strftime("%Y%m%d-%H%M%S", time.localtime(time_start))
-    file_stem = f'({time_ymdhms}){text[:Config.audio_name_len]}'
-    file_stem = re.sub(r'[\\/:"*?<>|]', ' ', file_stem)
+    file_stem = f"({time_ymdhms}){text[:Config.audio_name_len]}"
+    file_stem = re.sub(r'[\\/:"*?<>|]', " ", file_stem)
+    file_stem = re.sub(r"\r\n", " ", file_stem)
 
     # 重命名
     file_path_new = file_path.with_name(file_stem + file_path.suffix)
     file_path.rename(file_path_new)
-
     # 返回新的录音文件路径
     return file_path_new
