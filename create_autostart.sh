d690e7b5a14eefd9ce7d6e76915c2033b9c1e543
liuguangtian <toliuguangtian@gmail.com>
2026-01-26 18:57:07 +0800

    Restore autostart with terminal window using osascript

--- create_autostart.sh
+++ create_autostart.sh
@@ -36,8 +36,9 @@ cat > "$PLIST_FILE" << EOF
     
     <key>ProgramArguments</key>
     <array>
-        <string>/bin/bash</string>
-        <string>$PROJECT_DIR/launch_in_terminal.sh</string>
+        <string>/usr/bin/osascript</string>
+        <string>-e</string>
+        <string>tell application "Terminal" to do script "cd \"$PROJECT_DIR\" && bash \"$PROJECT_DIR/launch_in_terminal.sh\""</string>
     </array>
     
     <key>WorkingDirectory</key>
